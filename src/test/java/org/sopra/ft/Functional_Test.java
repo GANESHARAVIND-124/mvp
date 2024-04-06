47527b59480d8dbce9cc200f4d7e0b7c0975ca4e
jaisherma1 <jaisherma.j2019ece@sece.ac.in>
2024-04-06 18:35:58 +0530

    first commit

---
+++ src/test/java/org/sopra/ft/Functional_Test.java
@@ -0,0 +1,42 @@
+package org.sopra.ft;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class Functional_Test {
+
+	@Test
+	public void functionalTest() throws InterruptedException {
+		
+		
+		//options = Options()
+		//options.headless = True
+		//driver = webdriver.Chrome(options=options)
+		//driver.get("https://www.youtube.com/")
+		//element_text = driver.find_element_by_id("title").text
+		//print(element_text)
+	System.setProperty("webdriver.chrome.driver", "C:\\Office Work\\Software\\chromedriver.exe");
+    	ChromeOptions options = new ChromeOptions();
+        //options.addArguments("--headless");
+        //options.addArguments("--disable-gpu");
+		WebDriver driver = new ChromeDriver();
+		// Launch website
+		driver.navigate().to("https://www.google.com/");
+		//driver.navigate().refresh();
+		TimeUnit.SECONDS.sleep(30);
+		// Click on the search text box and send value
+		//WebElement divElement = driver.findElement(By.id("header_h1"));
+		//String str = divElement.getText();
+		//System.out.println(str);
+		//assertEquals("Welcome to DI-India", str);
+	}
+}
